a4dd0de4348a4430d38cbd2329e993714558f42e
Semyon Minyushov <minyushov@mercdev.com>
2016-11-11 18:46:50 +0400

    Make ItemTransformation fields non-final to allow reusing the same instance later.

--- CarouselLayoutManager/carousel/src/main/java/com/azoft/carousellayoutmanager/ItemTransformation.java
+++ CarouselLayoutManager/carousel/src/main/java/com/azoft/carousellayoutmanager/ItemTransformation.java
@@ -1,16 +1,50 @@
 package com.azoft.carousellayoutmanager;
 
 public class ItemTransformation {
+	float mScaleX;
+	float mScaleY;
+	float mTranslationX;
+	float mTranslationY;
 
-    final float mScaleX;
-    final float mScaleY;
-    final float mTranslationX;
-    final float mTranslationY;
+	public ItemTransformation() {
+	}
 
-    public ItemTransformation(final float scaleX, final float scaleY, final float translationX, final float translationY) {
-        mScaleX = scaleX;
-        mScaleY = scaleY;
-        mTranslationX = translationX;
-        mTranslationY = translationY;
-    }
+	public ItemTransformation(final float scaleX, final float scaleY, final float translationX, final float translationY) {
+		mScaleX = scaleX;
+		mScaleY = scaleY;
+		mTranslationX = translationX;
+		mTranslationY = translationY;
+	}
+
+	public float getScaleX() {
+		return mScaleX;
+	}
+
+	public void setScaleX(float scaleX) {
+		this.mScaleX = scaleX;
+	}
+
+	public float getScaleY() {
+		return mScaleY;
+	}
+
+	public void setScaleY(float scaleY) {
+		this.mScaleY = scaleY;
+	}
+
+	public float getTranslationX() {
+		return mTranslationX;
+	}
+
+	public void setTranslationX(float mTranslationX) {
+		this.mTranslationX = mTranslationX;
+	}
+
+	public float getTranslationY() {
+		return mTranslationY;
+	}
+
+	public void setTranslationY(float mTranslationY) {
+		this.mTranslationY = mTranslationY;
+	}
 }
